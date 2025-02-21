3fbf4a8e7d5735bd721c3d1cf7250d51787baf02
Le Tan <tamlokveer@gmail.com>
2025-02-21 21:05:52 +0800

    fix

--- tests/parser/test_strike.c
+++ tests/parser/test_strike.c
@@ -1,19 +1,19 @@
 #include "test_utils.h"
 
 int main() {
-    TEST_AST("d~a~\n",
-        "var: Document[0,5)\n"
-        "  var: BlockList[0,5)\n"
-        "    var: Plain[0,5)\n"
-        "      var: Strike[1,4)\n");
+    TEST_AST("d~~a~~\n",
+        "var: Document[0,7)\n"
+        "  var: BlockList[0,7)\n"
+        "    var: Plain[0,7)\n"
+        "      nul: Strike[1,6)\n");
 
-    TEST_AST("~abc~def\n"
-        "~abc~ def",
-        "var: Document[0,18)\n"
-        "  var: BlockList[0,18)\n"
-        "    var: Plain[0,18)\n"
-        "      var: Strike[0,5)\n"
-        "      var: Strike[9,14)\n");
+    TEST_AST("~~abc~~def\n"
+        "~~abc~~ def",
+        "var: Document[0,22)\n"
+        "  var: BlockList[0,22)\n"
+        "    var: Plain[0,22)\n"
+        "      nul: Strike[0,7)\n"
+        "      nul: Strike[11,18)\n");
 
     return 0;
 }
